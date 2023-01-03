80f29679261fb6d2ebcdc7a5636143d887b38eda
CharMe1001 <stefan.moldoveanu@s.unibuc.ro>
2023-01-03 22:07:07 +0200

    created generic repository

---
+++ ClassLibrary/Repositories/GenericRep/GenericRepository.cs
@@ -0,0 +1,112 @@
+﻿using Discord_Copycat.Data;
+using Discord_Copycat.Models.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Repositories.GenericRep
+{
+    internal class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly DiscordContext _discordContext;
+        private readonly DbSet<TEntity> _table;
+
+        public GenericRepository(DiscordContext discordContext)
+        {
+            _discordContext = discordContext;
+            _table = _discordContext.Set<TEntity>();
+        }
+
+        public void Create(TEntity entity)
+        {
+            _discordContext.Add(entity);
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            await _discordContext.AddAsync(entity);
+        }
+
+        public void CreateRange(IEnumerable<TEntity> entities)
+        {
+            _discordContext.AddRange(entities);
+        }
+
+        public async Task CreateRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _discordContext.AddRangeAsync(entities);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _discordContext.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            _discordContext.RemoveRange(entities);
+        }
+
+        public TEntity? FindById(object id)
+        {
+            return _table.Find(id);
+        }
+
+        public async Task<TEntity?> FindByIdAsync(object id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+        public IQueryable<TEntity> GetAllAsQueryable()
+        {
+            return _table.AsQueryable();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            var allAsync = await _table.AsNoTracking().ToListAsync();
+            return allAsync;
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                return _discordContext.SaveChanges() > 0;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return false;
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            try
+            {
+                return await _discordContext.SaveChangesAsync() > 0;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return false;
+        }
+
+        public void Update(TEntity entity)
+        {
+            _table.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            _table.UpdateRange(entities);
+        }
+    }
+}
---
+++ ClassLibrary/Repositories/GenericRep/IGenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using Discord_Copycat.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Repositories.GenericRep
+{
+    internal interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<List<TEntity>> GetAllAsync();
+        IQueryable<TEntity> GetAllAsQueryable();
+
+        void Create(TEntity entity);
+        Task CreateAsync(TEntity entity);
+        void CreateRange(IEnumerable<TEntity> entities);
+        Task CreateRangeAsync(IEnumerable<TEntity> entities);
+
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        void Delete(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entities);
+
+        TEntity? FindById(object id);
+        Task<TEntity?> FindByIdAsync(object id);
+
+        bool Save();
+        Task<bool> SaveAsync();
+    }
+}
